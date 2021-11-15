28aef461d50117b6a80dcb96dea553a333cb45b5
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-11-15 23:18:29 +0700

    firebase added

--- client/src/pages/Contact/ContactForm.js
+++ client/src/pages/Contact/ContactForm.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import emailjs from 'emailjs-com'
-import axios from 'axios'
+import React, { useState } from "react";
+import styled from "styled-components";
+import emailjs from "emailjs-com";
+import axios from "axios";
 
 const FormStyle = styled.form`
   width: 100%;
@@ -28,7 +28,7 @@ const FormStyle = styled.form`
     min-height: 150px;
     resize: vertical;
   }
-  button[type= 'submit'] {
+  button[type="submit"] {
     background-color: white;
     color: black;
     font-size: 1rem;
@@ -45,84 +45,93 @@ const FormStyle = styled.form`
   .message__close {
     display: none;
   }
-`
+`;
 
 function ContactForm() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
-  const [sent, setSend] = useState(false)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [sent, setSend] = useState(false);
 
   const resetForm = () => {
-      setTimeout(() => {
-      setName('')
-      setEmail('')
-      setMessage('')
-    },1000)
-  }
+    setTimeout(() => {
+      setName("");
+      setEmail("");
+      setMessage("");
+    }, 1000);
+  };
 
   const sendEmail = async (e) => {
-    e.preventDefault()
-    setSend(true)
-  //   emailjs.sendForm('service_96qi7vo', 
-  //     'template_ldjh2lk', 
-  //     e.target, 
-  //     'user_g3cqWH8EbvclEe2pkBZA9'
-  //   ).then((response) => {
-  //     console.log(response)
-  //   }).catch((error) => console.log(error))
-    await axios.post('/api/form', {
-      name,
-      email,
-      message,
-    })
-    .then(resetForm())
-    .catch((error) => console.log(error))
-  }
+    e.preventDefault();
+    setSend(true);
+    //   emailjs.sendForm('service_96qi7vo',
+    //     'template_ldjh2lk',
+    //     e.target,
+    //     'user_g3cqWH8EbvclEe2pkBZA9'
+    //   ).then((response) => {
+    //     console.log(response)
+    //   }).catch((error) => console.log(error))
+    await axios
+      .post("https://covid-tracker-2912.herokuapp.com/api/form", {
+        name,
+        email,
+        message,
+      })
+      .then(resetForm())
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div>
       <FormStyle onSubmit={sendEmail}>
-        <div className='contactform__group'>
-          <label htmlFor='name'>
+        <div className="contactform__group">
+          <label htmlFor="name">
             Your Name
-            <input type='text' id='name' 
-              name='name' 
+            <input
+              type="text"
+              id="name"
+              name="name"
               value={name}
-              placeholder='write your name...'
+              placeholder="write your name..."
               onChange={(e) => setName(e.target.value)}
             />
           </label>
         </div>
-        <div className='contactform__group'>
-          <label htmlFor='email'>
+        <div className="contactform__group">
+          <label htmlFor="email">
             Your Email
-            <input type='text' id='email' 
-              email='email' 
+            <input
+              type="text"
+              id="email"
+              email="email"
               value={email}
-              placeholder='write your email...'
+              placeholder="write your email..."
               onChange={(e) => setEmail(e.target.value)}
             />
           </label>
         </div>
-        <div className='contactform__group'>
-          <label htmlFor='message'>
+        <div className="contactform__group">
+          <label htmlFor="message">
             Your Message
-            <textarea type='textarea' id='message' 
-              message='message' 
+            <textarea
+              type="textarea"
+              id="message"
+              message="message"
               value={message}
-              placeholder='write your message...'
+              placeholder="write your message..."
               onChange={(e) => setMessage(e.target.value)}
             />
           </label>
         </div>
-        <div className={sent ? 'message__sent': 'message__close'}>
+        <div className={sent ? "message__sent" : "message__close"}>
           Message has been sent !!
         </div>
-        <button type='submit' onSubmit={resetForm}>Send</button>
+        <button type="submit" onSubmit={resetForm}>
+          Send
+        </button>
       </FormStyle>
     </div>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
